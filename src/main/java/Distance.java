e5229fc52ec10cccda91af7c561e25d8b1b7c4d1
zakiryanova <zakiryanova@localhost>
2019-09-30 21:19:12 +0300

    Initial commit

---
+++ src/main/java/Distance.java
@@ -0,0 +1,10 @@
+public class Distance {
+    public static void main(String[] args) {
+        double fuelForHundredKm = 4.7;
+        double consumption = 27;
+        int distance = (int) (consumption / fuelForHundredKm * 100);
+
+        System.out.println(distance);
+    }
+}
+//https://dropmefiles.com/h8bnc
